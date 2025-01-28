df7846cb91574f500545a235e15bdcc0cd405a3f
j_rozmetov <j.rozmetov@uzinfocom.uz>
2025-01-28 10:55:04 +0500

    Add to api some columns

--- app/Services/CounterService.php
+++ app/Services/CounterService.php
@@ -11,16 +11,18 @@ class CounterService
     public function getInnCounter($request)
     {
         $counters = Counter::query()
-            ->select('serial_number', 'customer_id')
+            ->select('serial_number', 'customer_id','caliber')
             ->with(['customer' => function ($query) {
-                $query->select('id', 'organization_INN'); // Select only the fields you need from the customer relation
+                $query->select('id', 'organization_INN','personal_account_number'); // Select only the fields you need from the customer relation
             }])
             ->whereIn('serial_number', $request->serialNumbers)
             ->get()
             ->map(function ($counter) {
                 return [
                     'serial_number' => $counter->serial_number,
+                    'caliber' => $counter->caliber,
                     'inn' => $counter->customer->organization_INN ?? null,
+                    'personalAccount' => $counter->customer->personal_account_number ?? null,
                 ];
             });
 
